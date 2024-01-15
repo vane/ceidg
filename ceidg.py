e18cf7347cc54189445baba4b94d363dc0e3ff3f
Michal Szczepanski <michal@vane.pl>
2024-01-15 11:38:38 +0100

    fix: url

--- ceidg.py
+++ ceidg.py
@@ -6,7 +6,8 @@ from datetime import datetime
 import requests
 import urllib3
 
-url = 'http://prod.ceidg.gov.pl/ceidg.cms.engine/Template/Includes/StatisticPage.aspx?Id=3814CF7F-246D-4CC3-8B89-88AA1395DF1D'
+url = 'https://prod.ceidg.gov.pl/ceidg.cms.engine/template/includes/statisticpage.aspx?id=3814cf7f-246d-4cc3-8b89-88aa1395df1d'
+
 resp = requests.get(url)
 soup = BeautifulSoup(resp.content, 'html.parser')
 tdlist = soup.find_all('td')
@@ -16,6 +17,8 @@ suspended = tdlist[5].text.strip()
 closed = tdlist[7].text.strip()
 
 all = BeautifulSoup(str(soup.find_all(string=lambda text: isinstance(text, Comment))[0]), 'html.parser').find_all('td')[1].text.strip()
+
+print(registered, renewed, suspended, closed, all)
 
 dt = datetime.now()
 fname = dt.strftime('%Y-%m-%d')
